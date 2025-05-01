c646c996f30445ce72ed2cf2e0445eba8860f480
bruno-dv2 <brunoge3@edu.unifor.br>
2025-05-01 16:49:58 -0300

    Finaliza backend e corrige dependências

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from '../../prisma/prisma.module';
 import { AuthModule } from '../auth/auth.module';
+import { EmpresasModule } from '../empresas/empresas.module';
+import { LocaisModule } from '../locais/locais.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    AuthModule,
+    EmpresasModule,
+    LocaisModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/locais/locais.module.ts
+++ backend/src/locais/locais.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { LocaisController } from './locais.controller';
 import { LocaisService } from './locais.service';
+import { PrismaModule } from 'prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [LocaisController],
-  providers: [LocaisService]
+  providers: [LocaisService],
+  exports: [LocaisService],
 })
 export class LocaisModule {}
