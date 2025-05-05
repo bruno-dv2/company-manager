682d3d28ebcce931c0255b64cc0f2e27db8809cf
bruno-dv2 <brunoge3@edu.unifor.br>
2025-05-05 12:00:38 -0300

    adiciona configuração Docker para backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,11 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'http://localhost:3003'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3001);
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
+  console.log(`Aplicação rodando na porta ${port}`);
 }
 void bootstrap();
